63069a0b323fda2c48080696478ee6550dcb7aaf
BenoitFaure <benoit.faure.cesar@gmail.com>
2025-07-05 01:41:48 +0200

    looking at json generation

---
+++ application/back_environment.py
@@ -0,0 +1,15 @@
+# Google
+PROJECT_ID = 'gemma-hcls25par-722' # Google Cloud Project ID
+REGION = 'us-central1' # Google Cloud Region
+
+# MedGemma Endpoint
+MEDGEMMA_ENDPOINT_ID = "5586325011125239808" # MedGemma Endpoint ID
+MEDGEMMA_ENDPOINT_REGION = "us-central1" # MedGemma Endpoint Region
+
+# MedGemma Finetuned Endpoint
+MEDGEMMA_FT_ENDPOINT_ID = "	1219240747459411968" # MedGemma Endpoint ID
+MEDGEMMA_FT_ENDPOINT_REGION = "us-central1" # MedGemma Endpoint Region
+
+# NnUnet Endpoint
+NNUNET_ENDPOINT_ID = "" # NnUnet Endpoint ID
+NNUNET_ENDPOINT_REGION = "us-central1" # NnUnet Endpoint Region
--- application/back_report.py
+++ application/back_report.py
@@ -1,8 +1,16 @@
-
-
 # GLOBAL VARIABLES
 MRI_FOLDER = "../front/public/mri"
 
 
 
 # GENERATE INFO FILE
+
+
+# Generates a Json with the following information:
+# - Client Info
+# - IRM Analysis
+# - Segmentation Analysis
+# - Stats
+# - MedGemma Analysis
+
+# Much of the information is contained in frontend
